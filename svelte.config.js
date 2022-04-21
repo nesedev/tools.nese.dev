cabae74b61bdfd8c37928b0dd3221d15266e9304
Nesewebel <20386898+Nesewebel@users.noreply.github.com>
2022-04-21 03:23:52 +0200

    Setup postcss and gh-pages deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,29 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer';
+
+const dev = process.env.NODE_ENV == 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  }),
 
   kit: {
     adapter: adapter(),
+
+    prerender: {
+      default: true,
+    },
+
+    paths: {
+      base: dev ? '' : '/modrinth-web-manager',
+    },
   },
 };
 
