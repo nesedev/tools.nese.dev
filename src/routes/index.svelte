8eef4278db39a71dff7d66d6051c0204a51c8fda
Nesewebel <20386898+Nesewebel@users.noreply.github.com>
2022-04-21 04:13:35 +0200

    Add second page to test routing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,11 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<a href="{base}/test">Test</a>
 
 <style lang="scss">
   p {
---
+++ src/routes/test.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
+<main>
+  <h1>Test</h1>
+  <a href="{base}/">Home</a>
+</main>
