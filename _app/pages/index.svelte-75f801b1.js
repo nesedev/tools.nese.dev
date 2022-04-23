0ff001f7f92fb576bcda6fe11b4c80e0f28fe3f9
Nesewebel <20386898+Nesewebel@users.noreply.github.com>
2022-04-23 05:52:50 +0200

    Updates

---
+++ _app/pages/index.svelte-75f801b1.js
@@ -0,0 +1,37 @@
+import{S as j,i as M,s as Q,w as H,k as q,e as m,t as b,x as W,m as h,c as l,a as c,h as _,d as n,b as U,y as k,g as I,G as t,E as B,q as G,o as R,B as z,H as J}from"../chunks/index-784640fd.js";import{c as r}from"../chunks/singletons-d1fb5791.js";import{b as K}from"../chunks/paths-396f020f.js";import{p as O}from"../chunks/stores-17ad4a3f.js";import{T as X}from"../chunks/title-fe6b8066.js";r.disable_scroll_handling;const Y=r.goto;r.invalidate;r.prefetch;r.prefetch_routes;r.before_navigate;r.after_navigate;function Z(x){let a,o,e,u,p,N,d,S,$,v,E,P,g,V,A,f,F,y;return a=new X({props:{text:"Home"}}),{c(){H(a.$$.fragment),o=q(),e=m("div"),u=m("h1"),p=b("Welcome to Nese Tools"),N=q(),d=m("p"),S=b("This page is very empty so please enjoy some of my favourite Lorem ipsum."),$=q(),v=m("p"),E=b(`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus magna, sollicitudin a dolor
+    vitae, congue tristique metus. Suspendisse potenti. Quisque sed diam quam. Etiam facilisis dui a
+    nulla convallis, id dignissim lorem lacinia. Nunc in enim purus. Ut pharetra turpis gravida
+    neque luctus, et ornare velit malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing
+    elit.`),P=q(),g=m("p"),V=b(`Sed condimentum pharetra tristique. Morbi pellentesque eros a lacus consequat, vitae maximus
+    justo dictum. Suspendisse potenti. Suspendisse tincidunt urna elementum, accumsan purus ac,
+    dapibus enim. Nam dictum amogus sussy nisi nec suscipit ultricies. Mauris finibus pellentesque
+    tristique. Fusce congue elit eget mauris vestibulum rhoncus. Aliquam erat volutpat. Donec
+    egestas rutrum massa. Fusce sapien risus, varius ut condimentum sed, cursus at quam. In et quam
+    a mauris pharetra auctor. Suspendisse potenti. Nunc interdum ac ex sed elementum. Donec mattis
+    ex eros, a ultricies ex imperdiet eu. Proin malesuada sagittis fermentum.`),A=q(),f=m("p"),F=b(`Vestibulum lobortis ac lacus ut euismod. Proin imperdiet dui eros, non iaculis dolor euismod
+    nec. Fusce eget libero turpis. Nunc blandit mauris vel libero tristique scelerisque. Quisque in
+    enim ut arcu mattis ornare. Vestibulum mauris tellus, pellentesque nec sem a, porttitor pharetra
+    lectus. Aliquam convallis pretium placerat. Integer posuere sem ac ante tempus, vel molestie
+    nibh tempor. Aliquam consequat never gonna give you up elit eget sapien mattis pretium.
+    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras
+    magna enim, convallis quis risus sed, vestibulum never gonna let you down tempor eros. Etiam
+    fermentum in neque ut laoreet. Nullam commodo dui libero. Nam non ullamcorper urna. Nullam id
+    eleifend sapien, id euismod sapien.`),this.h()},l(s){W(a.$$.fragment,s),o=h(s),e=l(s,"DIV",{class:!0});var i=c(e);u=l(i,"H1",{});var L=c(u);p=_(L,"Welcome to Nese Tools"),L.forEach(n),N=h(i),d=l(i,"P",{});var T=c(d);S=_(T,"This page is very empty so please enjoy some of my favourite Lorem ipsum."),T.forEach(n),$=h(i),v=l(i,"P",{});var w=c(v);E=_(w,`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras risus magna, sollicitudin a dolor
+    vitae, congue tristique metus. Suspendisse potenti. Quisque sed diam quam. Etiam facilisis dui a
+    nulla convallis, id dignissim lorem lacinia. Nunc in enim purus. Ut pharetra turpis gravida
+    neque luctus, et ornare velit malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing
+    elit.`),w.forEach(n),P=h(i),g=l(i,"P",{});var C=c(g);V=_(C,`Sed condimentum pharetra tristique. Morbi pellentesque eros a lacus consequat, vitae maximus
+    justo dictum. Suspendisse potenti. Suspendisse tincidunt urna elementum, accumsan purus ac,
+    dapibus enim. Nam dictum amogus sussy nisi nec suscipit ultricies. Mauris finibus pellentesque
+    tristique. Fusce congue elit eget mauris vestibulum rhoncus. Aliquam erat volutpat. Donec
+    egestas rutrum massa. Fusce sapien risus, varius ut condimentum sed, cursus at quam. In et quam
+    a mauris pharetra auctor. Suspendisse potenti. Nunc interdum ac ex sed elementum. Donec mattis
+    ex eros, a ultricies ex imperdiet eu. Proin malesuada sagittis fermentum.`),C.forEach(n),A=h(i),f=l(i,"P",{});var D=c(f);F=_(D,`Vestibulum lobortis ac lacus ut euismod. Proin imperdiet dui eros, non iaculis dolor euismod
+    nec. Fusce eget libero turpis. Nunc blandit mauris vel libero tristique scelerisque. Quisque in
+    enim ut arcu mattis ornare. Vestibulum mauris tellus, pellentesque nec sem a, porttitor pharetra
+    lectus. Aliquam convallis pretium placerat. Integer posuere sem ac ante tempus, vel molestie
+    nibh tempor. Aliquam consequat never gonna give you up elit eget sapien mattis pretium.
+    Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras
+    magna enim, convallis quis risus sed, vestibulum never gonna let you down tempor eros. Etiam
+    fermentum in neque ut laoreet. Nullam commodo dui libero. Nam non ullamcorper urna. Nullam id
+    eleifend sapien, id euismod sapien.`),D.forEach(n),i.forEach(n),this.h()},h(){U(e,"class","wrapper svelte-1dbbu7p")},m(s,i){k(a,s,i),I(s,o,i),I(s,e,i),t(e,u),t(u,p),t(e,N),t(e,d),t(d,S),t(e,$),t(e,v),t(v,E),t(e,P),t(e,g),t(g,V),t(e,A),t(e,f),t(f,F),y=!0},p:B,i(s){y||(G(a.$$.fragment,s),y=!0)},o(s){R(a.$$.fragment,s),y=!1},d(s){z(a,s),s&&n(o),s&&n(e)}}}function ee(x,a,o){let e;J(x,O,p=>o(0,e=p));const u=e.url;return u.hash&&u.hash.substring(1).startsWith("discord-april-fools")&&Y(`${K}/discord-april-fools`),[]}class ne extends j{constructor(a){super(),M(this,a,ee,Z,Q,{})}}export{ne as default};
